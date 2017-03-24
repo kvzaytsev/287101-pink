cd75eaee0588c1867b7768ebcdb56d6407c8f56e
kvzaytsev <kirill.v.zaytsev@gmail.com>
2017-03-24 19:14:59 +0300

    Let the magic begin

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-console.log("it's working...");
+var CONTAINER_SELECTOR = ".map";
+
+var ADDRESS = {
+  lat: 59.9387942,
+  long: 30.3208946
+};
+
+var warn = function (message) {
+  console.warn(message);
+};
+
+if (document.readyState === "complete") {
+  init();
+} else {
+  document.addEventListener("DOMContentLoaded", init);
+}
+
+function init() {
+
+}
