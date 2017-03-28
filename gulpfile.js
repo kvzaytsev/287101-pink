a1e36639c8939c45802b931fdb66e248a4b60704
kvzaytsev <kirill.v.zaytsev@gmail.com>
2017-03-28 23:15:01 +0300

    The machines must suffergit add .!

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,31 @@ var precss = require("precss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var svgmin = require("gulp-svgmin");
+var svgstore = require("gulp-svgstore");
+var imagemin = require("gulp-imagemin");
+var run = require("run-sequence");
+var del = require("del");
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("assets", function () {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+    .pipe(gulp.dest("build"));
+});
+
 gulp.task("style", function() {
   gulp.src("postcss/style.css")
     .pipe(plumber())
@@ -14,15 +39,52 @@ gulp.task("style", function() {
       precss(),
       autoprefixer({browsers: [
         "last 2 versions"
-      ]})
+      ]}),
+      mqpacker({
+        sort: true
+      })
     ]))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("sprite", function () {
+  return gulp.src("build/img/icons/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("images", function () {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("build", function (fn) {
+  run(
+    "clean",
+    "assets",
+    "style",
+    "images",
+    "sprite",
+    fn
+  );
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
+    server: "build",
     notify: false,
     open: true,
     cors: true,
